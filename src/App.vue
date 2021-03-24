6dce0d9e91bca9259c914e46f8bdd4005f32fbd0
MarlonDener <m.dener01@hotmail.com>
2021-03-24 09:45:26 -0300

    Completed calculator

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+     <h1>Calculadora</h1>
+     <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+
+  }
+}
+</script>
+
+<style>
+  @font-face{
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono.ttf");
+  }
+  *{
+    font-family: "RobotoMono", monospace;
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  h1{
+    padding: 20px 0;
+    font-weight: lighter;
+  }
+
+  #app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color:#fff;
+    background: linear-gradient(to right, #30042b,#000000);
+  }
+  @media (max-width: 500px) {
+  .calculator {
+    padding: 2px 15px;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/Display.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="display">
+        {{value}}
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['value']   
+    }
+</script>
+
+<style>
+.display{
+    grid-column: span 4;
+    background-color: rgba(204, 204, 204, 0.267);
+    display: flex;
+    justify-content: flex-end;
+    padding: 30px 15px;
+    font-size: 1.8em;
+}
+
+</style>
